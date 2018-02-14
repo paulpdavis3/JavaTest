a3e63bf5d78b8fb4c9dd7c56f7919383a944678f
Paul Davis <C16324311@mydit.ie>
2018-02-14 17:08:40 +0000

    created the tunebook class and added the findtune method

---
+++ ie/dit/TuneBook.java
@@ -0,0 +1,15 @@
+package ie.dit;
+
+import java.util.ArrayList;
+import java.io.BufferedReader;
+
+
+public class TuneBook
+{
+	ArrayList<String> tunes = new ArrayList<String>();
+	
+	public Tune findTune(String title)
+	{
+		
+	}
+}
