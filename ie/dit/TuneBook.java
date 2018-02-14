ba965bb3af2b2f1745472d7bbeb4665a4c01a4a4
Paul Davis <C16324311@mydit.ie>
2018-02-14 17:12:08 +0000

    added the main method to the tunebook class

--- ie/dit/TuneBook.java
+++ ie/dit/TuneBook.java
@@ -18,4 +18,13 @@ public class TuneBook
 			}
         }
 	}
+	
+	public static void main(String[] args)
+    {
+        TuneBook tb = new TuneBook("hnj0.abc");
+        System.out.println(tb);
+
+        Tune t = tb.findTune("Scotsman over the Border");
+        t.play();
+    }
 }
