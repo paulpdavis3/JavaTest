587f74e330b0c3675d40a4efe25244506bf13bc6
Paul Davis <C16324311@mydit.ie>
2018-02-14 17:10:34 +0000

    completed the findtune method

--- ie/dit/TuneBook.java
+++ ie/dit/TuneBook.java
@@ -8,8 +8,14 @@ public class TuneBook
 {
 	ArrayList<String> tunes = new ArrayList<String>();
 	
-	public Tune findTune(String title)
+	public Tune findTune(String title) //finds the title of the song in the array list of tunes
 	{
-		
+		for(String tune:tunes)
+        {
+			if(tune.contains(title))
+			{
+				return title;
+			}
+        }
 	}
 }
