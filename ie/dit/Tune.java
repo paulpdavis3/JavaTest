47bb83443696f95c73aa224d9b472ca28a0baff8
Paul Davis <C16324311@mydit.ie>
2018-02-14 16:19:12 +0000

    created the private variables and getX public accessor method

---
+++ ie/dit/Tune.java
@@ -0,0 +1,14 @@
+package ie.dit;
+
+public class Tune
+{
+	private int x;
+	private String title;
+	private String altTitle;
+	private String notation;
+	
+	public int getX()
+	{
+		return x;
+	}
+}
