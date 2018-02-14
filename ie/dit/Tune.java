da31e2ed31103722334839e071ac76f9ba21eed6
Paul Davis <C16324311@mydit.ie>
2018-02-14 16:49:44 +0000

    attempted to fix toString method and added if statement to get all the index numbers of the tunes

--- ie/dit/Tune.java
+++ ie/dit/Tune.java
@@ -1,9 +1,6 @@
 package ie.dit;
 
-import java.io.IOException;
-import java.io.FileReader;
-import java.util.ArrayList;
-import java.io.BufferedReader;
+
 
 public class Tune
 {
@@ -12,6 +9,8 @@ public class Tune
 	private String altTitle;
 	private String notation;
 	
+	ArrayList<String> words = new ArrayList<String>();
+	
 	public int getX()
 	{
 		return x;
@@ -19,12 +18,55 @@ public class Tune
 	
 	public String toString()
     {
+		loadText();
+		
         StringBuffer sb = new StringBuffer();
         for(String word:words)
         {
-            sb.append(word + ", \n");
+			if(word.contains("X:"))
+			{
+				sb.append(word + ", \n");
+			}
         }
 
         return sb.toString();
 	}
+	
+	public void loadText()
+    {
+        BufferedReader inputStream = null;
+
+        try {
+            inputStream = new BufferedReader(new FileReader("hnj0.abc"));
+            
+            String l;
+            while ((l = inputStream.readLine()) != null) {
+                words.add(l);
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        finally 
+        {
+            if (inputStream != null) {
+                try
+                {
+                    inputStream.close();
+                }
+                catch(Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+	
+	 public static void main(String[] args)
+	{
+		Tune main = new Tune();
+		
+		main.toString();
+	} 
 }
